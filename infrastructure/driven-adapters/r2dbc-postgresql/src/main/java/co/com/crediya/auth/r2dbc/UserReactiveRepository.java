704799e9c07d9ae4a469aa644d2103ae857fd358
Juan Urrego <juan.u@hyros.com>
2025-08-25 23:12:19 -0400

    Base adapter and user repository

---
+++ infrastructure/driven-adapters/r2dbc-postgresql/src/main/java/co/com/crediya/auth/r2dbc/UserReactiveRepository.java
@@ -0,0 +1,14 @@
+package co.com.crediya.auth.r2dbc;
+
+import java.util.UUID;
+
+import org.springframework.data.repository.query.ReactiveQueryByExampleExecutor;
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+
+import co.com.crediya.auth.r2dbc.entity.UserEntity;
+import reactor.core.publisher.Mono;
+
+public interface UserReactiveRepository
+    extends ReactiveCrudRepository<UserEntity, UUID>, ReactiveQueryByExampleExecutor<UserEntity> {
+  Mono<Boolean> existsByEmail(String email);
+}
---
+++ infrastructure/driven-adapters/r2dbc-postgresql/src/main/java/co/com/crediya/auth/r2dbc/UserRepositoryAdapter.java
@@ -0,0 +1,34 @@
+package co.com.crediya.auth.r2dbc;
+
+import java.util.UUID;
+
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.reactive.TransactionalOperator;
+
+import co.com.crediya.auth.model.user.User;
+import co.com.crediya.auth.model.user.gateways.UserRepository;
+import co.com.crediya.auth.model.user.vo.UserEmail;
+import co.com.crediya.auth.r2dbc.entity.UserEntity;
+import co.com.crediya.auth.r2dbc.helper.ReactiveAdapterOperations;
+import co.com.crediya.auth.r2dbc.helper.UserMapperStandard;
+import reactor.core.publisher.Mono;
+
+@Repository
+public class UserRepositoryAdapter
+    extends ReactiveAdapterOperations<User, UserEntity, UUID, UserReactiveRepository>
+    implements UserRepository {
+  private final TransactionalOperator txOperator;
+
+  public UserRepositoryAdapter(
+      UserReactiveRepository repository,
+      UserMapperStandard mapper,
+      TransactionalOperator txOperator) {
+    super(repository, mapper::toDomain, mapper::toEntity);
+    this.txOperator = txOperator;
+  }
+
+  @Override
+  public Mono<Boolean> existsByEmail(UserEmail email) {
+    return repository.existsByEmail(email.value()).as(txOperator::transactional);
+  }
+}
