8061aa4354aa84a5a7837d70fe06717488152ba2
Juan Urrego <juan.u@hyros.com>
2025-08-25 22:50:27 -0400

    UserMapper Implementation

---
+++ infrastructure/driven-adapters/r2dbc-postgresql/src/main/java/co/com/crediya/auth/r2dbc/helper/UserMapperStandard.java
@@ -0,0 +1,77 @@
+package co.com.crediya.auth.r2dbc.helper;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+import co.com.crediya.auth.model.user.IdentificationType;
+import co.com.crediya.auth.model.user.User;
+import co.com.crediya.auth.model.user.dto.CreateUserCommand;
+import co.com.crediya.auth.model.user.dto.UserResponseDTO;
+import co.com.crediya.auth.model.user.mapper.UserMapper;
+import co.com.crediya.auth.model.user.vo.*;
+import co.com.crediya.auth.r2dbc.entity.UserEntity;
+
+@Mapper(componentModel = "spring")
+public interface UserMapperStandard extends UserMapper {
+  @Mapping(target = "email", source = "email.value")
+  @Mapping(target = "firstName", source = "firstName.value")
+  @Mapping(target = "lastName", source = "lastName.value")
+  @Mapping(target = "baseSalary", source = "baseSalary.value")
+  @Mapping(target = "idNumber", source = "idNumber.value")
+  @Mapping(target = "birthDate", source = "birthDate.value")
+  @Mapping(target = "phoneNumber", source = "phoneNumber.value")
+  @Mapping(target = "address", source = "address.value")
+  UserEntity toEntity(User domain);
+
+  User toDomain(UserEntity entity);
+
+  @Override
+  @Mapping(target = "id", ignore = true)
+  User toDomainCommand(CreateUserCommand command);
+
+  @Override
+  @Mapping(target = "email", source = "email.value")
+  @Mapping(target = "firstName", source = "firstName.value")
+  @Mapping(target = "idNumber", source = "idNumber.value")
+  @Mapping(target = "idType", source = "idType.code")
+  UserResponseDTO toDTO(User user);
+
+  default UserEmail toUserEmail(String email) {
+    return new UserEmail(email);
+  }
+
+  default FirstName toFirstName(String firstName) {
+    return new FirstName(firstName);
+  }
+
+  default LastName toLastName(String lastName) {
+    return new LastName(lastName);
+  }
+
+  default BirthDate toBirthDate(LocalDate date) {
+    return new BirthDate(date);
+  }
+
+  default Address toAddress(String address) {
+    return new Address(address);
+  }
+
+  default UserSalary toUserSalary(BigDecimal salary) {
+    return new UserSalary(salary);
+  }
+
+  default PhoneNumber toPhoneNumber(String phoneNumber) {
+    return new PhoneNumber(phoneNumber);
+  }
+
+  default IdentificationType toIdType(String idType) {
+    return IdentificationType.fromCode(idType);
+  }
+
+  default IdNumber toIdNumber(String idNumber) {
+    return new IdNumber(idNumber);
+  }
+}
