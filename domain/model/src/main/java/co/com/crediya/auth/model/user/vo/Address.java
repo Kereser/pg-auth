4165251c92db974eef8af603ef7076e9d221f980
Juan Urrego <juan.u@hyros.com>
2025-08-29 19:59:14 -0400

    Update VOs with new exception values

--- domain/model/src/main/java/co/com/crediya/auth/model/user/vo/Address.java
+++ domain/model/src/main/java/co/com/crediya/auth/model/user/vo/Address.java
@@ -1,24 +1,22 @@
 package co.com.crediya.auth.model.user.vo;
 
-import co.com.crediya.auth.model.user.exceptions.IllegalValueForArgumentException;
-import co.com.crediya.auth.model.user.exceptions.MissingValueOnRequiredFieldException;
+import co.com.crediya.auth.model.user.exceptions.*;
 
 public record Address(String value) {
-  private static final String FIELD = "address";
   private static final int MIN_LENGTH = 5;
   private static final int MAX_LENGTH = 80;
-  private static final String LENGTH_EXCEPTION = "Address length must be between %d and %d";
-  private static final String MUST_HAVE_VALUE = "Address must have a value if its present.";
 
   public Address {
     if (value != null) {
       if (value.isBlank()) {
-        throw new MissingValueOnRequiredFieldException(FIELD, MUST_HAVE_VALUE);
+        throw new MissingValueOnRequiredFieldException(
+            Fields.ADDRESS.getName(), PlainErrors.NOT_EMPY.getMsg());
       }
 
       if (value.trim().length() > MAX_LENGTH || value.trim().length() < MIN_LENGTH) {
         throw new IllegalValueForArgumentException(
-            FIELD, String.format(LENGTH_EXCEPTION, MIN_LENGTH, MAX_LENGTH));
+            Fields.ADDRESS.getName(),
+            TemplateErrors.LENGTH_BOUNDARIES.buildMsg(MIN_LENGTH, MAX_LENGTH));
       }
     }
   }
--- domain/model/src/main/java/co/com/crediya/auth/model/user/vo/IdNumber.java
+++ domain/model/src/main/java/co/com/crediya/auth/model/user/vo/IdNumber.java
@@ -1,23 +1,20 @@
 package co.com.crediya.auth.model.user.vo;
 
-import co.com.crediya.auth.model.user.exceptions.IllegalValueForArgumentException;
-import co.com.crediya.auth.model.user.exceptions.MissingValueOnRequiredFieldException;
+import co.com.crediya.auth.model.user.exceptions.*;
 
 public record IdNumber(String value) {
-  private static final String FIELD = "idNumber";
   private static final int MIN_VALUE = 5;
   private static final int MAX_VALUE = 20;
-  private static final String VALUE_LENGTH = "idNumber values must be between %d and %d.";
-  private static final String NOT_EMPTY = "Must not be empty";
 
   public IdNumber {
     if (value == null || value.isBlank()) {
-      throw new MissingValueOnRequiredFieldException(FIELD, NOT_EMPTY);
+      throw new MissingValueOnRequiredFieldException(
+          Fields.ID_NUMBER.getName(), PlainErrors.NOT_EMPY.getMsg());
     }
 
     if (value.trim().length() < MIN_VALUE || value.trim().length() > MAX_VALUE) {
       throw new IllegalValueForArgumentException(
-          FIELD, String.format(VALUE_LENGTH, MIN_VALUE, MAX_VALUE));
+          Fields.ID_NUMBER.getName(), TemplateErrors.LENGTH_BOUNDARIES.buildMsg(MIN_VALUE, MAX_VALUE));
     }
   }
 }
