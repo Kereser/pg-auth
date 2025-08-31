dc620b528661ce47caca367f57d98e40d373d584
Juan Urrego <juan.u@hyros.com>
2025-08-30 22:46:59 -0400

    Update use case to handle hashing password and save default CLIENT role

--- domain/usecase/src/main/java/co/com/crediya/auth/usecase/createuser/CreateUserUseCaseImp.java
+++ domain/usecase/src/main/java/co/com/crediya/auth/usecase/createuser/CreateUserUseCaseImp.java
@@ -1,11 +1,13 @@
 package co.com.crediya.auth.usecase.createuser;
 
+import co.com.crediya.auth.model.helper.PasswordEncoder;
+import co.com.crediya.auth.model.role.Role;
+import co.com.crediya.auth.model.role.RoleName;
+import co.com.crediya.auth.model.role.gateways.RoleRepository;
 import co.com.crediya.auth.model.user.User;
 import co.com.crediya.auth.model.user.dto.CreateUserCommand;
 import co.com.crediya.auth.model.user.dto.UserResponseDTO;
-import co.com.crediya.auth.model.user.exceptions.DuplicatedInfoException;
-import co.com.crediya.auth.model.user.exceptions.Fields;
-import co.com.crediya.auth.model.user.exceptions.TemplateErrors;
+import co.com.crediya.auth.model.user.exceptions.*;
 import co.com.crediya.auth.model.user.gateways.UserRepository;
 import co.com.crediya.auth.model.user.mapper.UserMapper;
 import lombok.RequiredArgsConstructor;
@@ -14,6 +16,8 @@ import reactor.core.publisher.Mono;
 @RequiredArgsConstructor
 public class CreateUserUseCaseImp implements CreateUserUseCase {
   private final UserRepository userRepository;
+  private final RoleRepository roleRepository;
+  private final PasswordEncoder passwordEncoder;
   private final UserMapper userMapper;
 
   @Override
@@ -21,8 +25,30 @@ public class CreateUserUseCaseImp implements CreateUserUseCase {
     User newUser = userMapper.toDomainFromCommand(command);
 
     return Mono.when(validateUniqueUser(newUser), validateUniqueIdNumber(newUser))
-        .then(Mono.defer(() -> userRepository.save(newUser)))
+        .then(
+            Mono.defer(
+                () -> {
+                  User user = this.hashPassword(newUser);
+
+                  return saveUserWithClientRole(user);
+                }))
         .map(userMapper::toDTOResponse);
+  }
+
+  private Mono<User> saveUserWithClientRole(User user) {
+    return roleRepository
+        .findClientRole()
+        .switchIfEmpty(
+            Mono.error(
+                new EntityNotFoundException(
+                    Entities.ROLE.name(),
+                    TemplateErrors.X_NOT_FOUND_FOR_Y.buildMsg(
+                        Entities.ROLE.name(), RoleName.CLIENT.getName()))))
+        .flatMap(role -> saveUserWithGivenRole(user, role));
+  }
+
+  private Mono<User> saveUserWithGivenRole(User user, Role role) {
+    return userRepository.save(user.toBuilder().role(role).build());
   }
 
   private Mono<Void> validateUniqueUser(User user) {
@@ -50,5 +76,9 @@ public class CreateUserUseCaseImp implements CreateUserUseCase {
                             Fields.ID_NUMBER.getName(),
                             TemplateErrors.X_ALREADY_IN_USER.buildMsg(Fields.ID_NUMBER.getName())))
                     : Mono.empty());
+  }
+
+  private User hashPassword(User user) {
+    return user.toBuilder().password(passwordEncoder.encode(user.getPassword())).build();
   }
 }
