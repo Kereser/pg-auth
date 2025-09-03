42ebbd66604b936aac72aee0a1f4b0684cc3822e
Juan Urrego <juan.u@hyros.com>
2025-09-03 17:36:58 -0400

    Add request dto and update mapper to handle it

--- infrastructure/entry-points/reactive-web/src/main/java/co/com/crediya/auth/api/helper/UserRestMapper.java
+++ infrastructure/entry-points/reactive-web/src/main/java/co/com/crediya/auth/api/helper/UserRestMapper.java
@@ -4,9 +4,13 @@ import org.mapstruct.Mapper;
 import org.mapstruct.MappingConstants;
 
 import co.com.crediya.auth.api.dto.CreateUserDTORequest;
+import co.com.crediya.auth.api.dto.FindAllUsersFiltersDTORequest;
 import co.com.crediya.auth.model.user.dto.CreateUserCommand;
+import co.com.crediya.auth.model.user.dto.FindAllUsersFiltersCommand;
 
 @Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
 public interface UserRestMapper {
   CreateUserCommand toCommand(CreateUserDTORequest dto);
+
+  FindAllUsersFiltersCommand toCommand(FindAllUsersFiltersDTORequest dto);
 }
